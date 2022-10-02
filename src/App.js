f00983eb130f065d53a690be8afc6b0f52f5790c
sellennya <57621268+sellennya@users.noreply.github.com>
2022-10-02 10:57:13 +0100

    add search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className='App'>
-      <header className='App-header'>Hello World</header>
+      <div className='container'>
+        <header className='App-header'>
+          <h1>Dictionary Project</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className='App-footer'>
+          <small>Coded by Sandra Cardoso</small>
+        </footer>
+      </div>
     </div>
   );
 }
