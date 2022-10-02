cc5a97125d9e712ddd99e44f9cb8f0050bedbbf1
sellennya <57621268+sellennya@users.noreply.github.com>
2022-10-02 11:20:18 +0100

    add api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,18 @@
 import React, { useState } from 'react';
 import './Dictionary.css';
+import axios from 'axios';
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState('');
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
   function habndleKeywordChange(event) {
     setKeyword(event.target.value);
